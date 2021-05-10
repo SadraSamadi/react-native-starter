22e7d68e31de77a1f73d0420853a92342b9b64d1
Sadra Samadi <sadra.smd@gmail.com>
2021-05-10 23:57:58 +0430

    added navigation

--- android/app/src/main/java/com/sadrasamadi/myapplication/MainActivity.java
+++ android/app/src/main/java/com/sadrasamadi/myapplication/MainActivity.java
@@ -1,15 +1,33 @@
 package com.sadrasamadi.myapplication;
 
-import androidx.annotation.Nullable;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
-  @Nullable
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "main";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    String name = getMainComponentName();
+    return new ReactActivityDelegate(this, name) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
 }
--- android/app/src/main/java/com/sadrasamadi/myapplication/MainApplication.java
+++ android/app/src/main/java/com/sadrasamadi/myapplication/MainApplication.java
@@ -2,13 +2,53 @@ package com.sadrasamadi.myapplication;
 
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
+import com.sadrasamadi.myapplication.generated.BasePackageList;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.Package;
+
+import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private MainReactNativeHost mMainReactNativeHost;
+  private ReactModuleRegistryProvider mModuleRegistryProvider;
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+
+    @Override
+    protected JSIModulePackage getJSIModulePackage() {
+      return new ReanimatedJSIModulePackage();
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      PackageList list = new PackageList(this);
+      List<ReactPackage> packages = list.getPackages();
+      ReactModuleRegistryProvider provider = getModuleRegistryProvider();
+      ModuleRegistryAdapter adapter = new ModuleRegistryAdapter(provider);
+      packages.add(adapter);
+      return packages;
+    }
+
+  };
 
   @Override
   public void onCreate() {
@@ -18,9 +58,16 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    if (mMainReactNativeHost == null)
-      mMainReactNativeHost = new MainReactNativeHost(this);
-    return mMainReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  private ReactModuleRegistryProvider getModuleRegistryProvider() {
+    if (mModuleRegistryProvider == null) {
+      BasePackageList list = new BasePackageList();
+      List<Package> packages = list.getPackageList();
+      mModuleRegistryProvider = new ReactModuleRegistryProvider(packages);
+    }
+    return mModuleRegistryProvider;
   }
 
 }
